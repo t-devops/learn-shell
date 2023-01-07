f6e102de4d3f2f869628c76406637e4cedc3a321
ThReddy <devopst70@gmail.com>
2023-01-07 11:16:21 -0600

    Unix basic examples

---
+++ 03-example.sh
@@ -0,0 +1,12 @@
+## If we assign a name to a set of data is called a variable.
+trainer=Raghu
+class=DevOps
+echo Trainer Name - $trainer
+echo Class Name - ${class}
+
+#DATE=2023-01-03  (this is hard coded)
+DATE=$(date +%F)   #(dynamic)
+echo Hey, Today date is $DATE
+
+ADD=$((2+3))
+echo ADD - $ADD
