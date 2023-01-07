363e39b42b504c10326f1efca06c15373fbba508
ThReddy <devopst70@gmail.com>
2023-01-07 11:12:08 -0600

    Hello

--- 02-example.sh
+++ 02-example.sh
@@ -1,2 +1,20 @@
 echo Hello World
 echo "***Hello World***"
+
+You can give color to the messages, Unix supports 6 colors
+Red   Green   Yellow  Blue  Magenta Cyan
+Syntax:
+# Color Code Syntax : echo -e "\e[COLmMESSAGE\e[0m"
+# Red - 31 - echo -e "\e[31mMESSAGE\e[0m"
+# Green - 32 - echo -e "\e[32mMESSAGE\e[0m"
+# Yellow - 33 - echo -e "\e[33mMESSAGE\e[0m"
+# Blue - 34 - echo -e "\e[34mMESSAGE\e[0m"
+# Magenta - 35 - echo -e "\e[35mMESSAGE\e[0m"
+# Cyan - 36 - echo -e "\e[36mMESSAGE\e[0m"
+
+echo -e "\e[31mMESSAGE\e[0m"
+echo -e "\e[32mMESSAGE\e[0m"
+echo -e "\e[33mMESSAGE\e[0m"
+echo -e "\e[34mMESSAGE\e[0m"
+echo -e "\e[35mMESSAGE\e[0m"
+echo -e "\e[36mMESSAGE\e[0m"
